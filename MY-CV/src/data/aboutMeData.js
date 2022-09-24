335d8347c4b1dcc9081fd20993c98e7e4052f138
IhebMasmoudi <i.masmoudi19045@pi.tn>
2022-09-24 21:13:28 +0100

    first commit

---
+++ MY-CV/src/data/aboutMeData.js
@@ -0,0 +1,9 @@
+export const aboutMeData = [
+	{
+		id: 1,
+		bio : "Student at Polytech INT, Tunis, dynamic and extremely motivated to invest in the field of IT development. Creativity, rigor and responsibility characterize my line of conduct in life. I would like to have the chance to confront my skills with reality and to discover the professional world and its actors and take a first step towards them"},
+	{
+		id: 2,
+		bio: 'Throughout the year I developed a sense of joy in using my academic and professional skills in my self-improvement journey. I firmly believe that my professional path is a highway to personal growth, this explains my thirst for knowledge. This thirst fuels my discipline, curiosity, authenticity, and motivations.',
+	},
+];
---
+++ MY-CV/src/data/projects.js
@@ -0,0 +1,40 @@
+// Import images
+import WebImage1 from '../images/web-project-1.jpg';
+import WebImage2 from '../images/web-project-2.jpg';
+import MobileImage1 from '../images/mobile-project-1.jpg';
+import MobileImage2 from '../images/mobile-project-2.jpg';
+import UIImage1 from '../images/ui-project-1.jpg';
+import UIImage2 from '../images/ui-project-2.jpg';
+
+export const projectsData = [
+	{
+		id: 1,
+		title: 'SMART OGSO SELECTOR',
+		category: 'Web Application',
+		img: WebImage2,
+		ProjectHeader: {
+			title: 'Project Management UI - From Context',
+			publishDate: 'Jul 26, 2021',
+			tags: 'UI / Frontend',
+		},
+	},
+	{
+		id: 2,
+		title: 'THE MARTIAN',
+		category: 'Mobile Application',
+		img: MobileImage2,
+	},
+	{
+		id: 3,
+		title: 'OGSO MOUNTAINS',
+		category: 'WORDPRESS WEBSITE',
+		img: UIImage1,
+	},
+	{
+		id: 4,
+		title: 'HUILE PHARAONS',
+		category: 'WORDPRESS WEBSITE',
+		img: UIImage2,
+	},
+	
+];
